b27a2b514717e45e737bf6fc7f937e48b7444114
Andrei Cioban <andrei.cioban@assist.ro>
2017-11-02 22:57:18 +0200

    Allow x-auth header on cors

--- src/config/express.ts
+++ src/config/express.ts
@@ -25,7 +25,7 @@ export class App {
         this.app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
             res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth");
             next();
          });
     }
